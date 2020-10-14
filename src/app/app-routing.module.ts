7fe7263c39905c695aa5f20adb2d890721a7ad15
juned <junednss@gmail.com>
2020-10-13 22:25:22 -0700

    {{mam work pull}}

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,16 @@ import {HomeComponent} from './home/home.component';
 import {CategoryPageComponent} from './category-page/category-page.component';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
-
+import {ProductViewComponent} from './product-view/product-view.component';
+ 
 const routes: Routes = [
   { path:'', redirectTo:'/home', pathMatch:'full'},
  
   {  path:"header",component : HeaderComponent },
   {  path:"home",component : HomeComponent },
   {  path:"footer",component : FooterComponent },
-  {  path:"category-page",component : CategoryPageComponent }
-  
+  {  path:"category-page",component : CategoryPageComponent },
+  { path:"product-view",component : ProductViewComponent}
 ];
 
 @NgModule({
