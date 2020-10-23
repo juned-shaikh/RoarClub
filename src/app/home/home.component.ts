8a8c9bd4a8a845130fdd7556cc09227a99c87146
juned <junednss@gmail.com>
2020-10-23 20:19:43 +0530

    home page responsive, search bar issue solve

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1404,6 +1404,7 @@ nav.replace(/\s+/g, "-") +
       comp_num: sessionStorage.getItem("comp_num_new"),
       access_token: sessionStorage.getItem("access_token")
     }
+    
 
       this.adminservice.updateCart(data2).subscribe(data => {
        
